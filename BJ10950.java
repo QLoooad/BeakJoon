b4e655835f4e2a5937f622d6070af04a24aaab2a
QLoooad <ckcc0813@gmail.com>
2022-09-26 20:57:45 +0900

    BeakJoon

---
+++ BJ10950.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+ 
+public class BJ10950 {
+ 
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+        
+		int a = in.nextInt();
+		
+		
+		
+		for(int i = 0; i < a; i++ ) {
+			int A = in.nextInt();
+			int B = in.nextInt();
+			System.out.println(A+B);
+		}
+		
+		in.close();
+	}	
+	
+}
---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+        Scanner Multi = new Scanner(System.in);
+        
+        int A = Multi.nextInt();
+		int B = Multi.nextInt();
+        
+		Multi.close();
+		
+		if(A>B) System.out.println(">");
+		else if(A<B) System.out.println("<");
+		else System.out.println("==");
+		
+		
+	}
+}
