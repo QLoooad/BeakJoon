fa10baa26b65454fba795d5effe0af1d114eab38
QLoooad <ckcc0813@gmail.com>
2022-09-26 21:42:38 +0900

    메뉴 추가

--- BJ2739_구구단.java
+++ BJ2739_구구단.java
@@ -4,12 +4,16 @@ import java.io.IOException;
  
 public class BJ2739_구구단 {
 	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int a = Integer.parseInt(br.readLine());	
-		br.close();
+		System.out.println("원하시는 단수를 입력하세요.");
+		BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+		int a = Integer.parseInt(br1.readLine());	
+		System.out.println("어디까지 곱할지 입력해주세요.");
+		BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
+		int b = Integer.parseInt(br2.readLine());
+		br1.close();
+		br2.close();
         
-		for(int i = 1; i<10;i++) {
+		for(int i = 0; i<=b;i++) {
 			System.out.println(a+" * "+i+" = "+(a*i));
 		}
 	}
