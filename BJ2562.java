11845e1de04a09c8d3fd338dd88d99a1ecd7a0f2
QLoooad <ckcc0813@gmail.com>
2022-10-06 18:36:18 +0900

    221006 해석추가

--- BJ2562.java
+++ BJ2562.java
@@ -18,16 +18,21 @@ public class BJ2562 {
 		int max = 0;
 		int count = 0;
 		int index = 0;
-		for(int arr2 : arr) {
+		for(int arr2 : arr) { 
 			count++;
 			if(arr2 > max) {
 				max = arr2;
 				index = count;
-			}
+			} 
 		}
-		
 		System.out.println(max);
 		System.out.println(index);
 	}
-
 }
+/*
+ * 2차 for문 count 왜 index로 복제하는가
+ * count를 if에 넣으면 arr를 비교할때 중간에 비교하는 수 보다 작은수가 있으면
+ * 위치정보 카운트를 하지 않기에 for문에 count를 넣고
+ * if를 지날때마다 위치정보 카운트가 올라가게 한다음 해당 수를 index에 넣어서 
+ * 끝까지 증가(arr의 총 개수만큼)하는 count에 대비하기 위함  
+ */
