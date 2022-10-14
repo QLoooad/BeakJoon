a5f47ac209184c5f85dc65ff66279a01e8554a5a
QLoooad <ckcc0813@gmail.com>
2022-10-14 21:02:18 +0900

    221013

--- BJ2675.java
+++ BJ2675.java
@@ -8,6 +8,19 @@ public class BJ2675 {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int TestCase = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<TestCase; i++) {
+			String[] arr = br.readLine().split(" ");
+			
+			int Re = arr[0].charAt(0)-'0';
+			String S = arr[1];
+			
+			for(int j=0; j<S.length(); j++) {
+				for(int k=0; k<Re; k++) {
+					System.out.print(S.charAt(j));
+				}
+			}
+			System.out.println();
+		}
 	}
-
 }
