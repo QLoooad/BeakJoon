6d7962ff76f8e7cbd861901908a0c935ac1ff08f
QLoooad <ckcc0813@gmail.com>
2023-02-07 09:29:49 +0900

    230206

---
+++ PG_제일작은수제거.java
@@ -0,0 +1,57 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class PG_제일작은수제거 {
+
+	public static int[] solution(int[] arr) {
+
+		int[] arr2 = arr.clone();
+
+		Arrays.sort(arr);
+		int min = arr[0];
+
+		ArrayList<Integer> intArr = new ArrayList<Integer>();
+		for (int ExArr : arr2) {
+			intArr.add(ExArr);
+		}
+		if (arr.length > 1) {
+			intArr.remove(Integer.valueOf(min));
+		} else {
+			intArr.set(0, -1);
+		}
+
+		int[] result = new int[intArr.size()];
+
+		for (int i = 0; i < intArr.size(); i++) {
+			result[i] = intArr.get(i);
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 5, 3, 1, 4, 2 };
+		int[] arr2 = arr.clone();
+
+		Arrays.sort(arr);
+		int min = arr[0];
+
+		ArrayList<Integer> intArr = new ArrayList<Integer>();
+		for (int ExArr : arr2) {
+			intArr.add(ExArr);
+		}
+		if (arr.length > 1) {
+			intArr.remove(Integer.valueOf(min));
+		} else {
+			intArr.set(0, -1);
+		}
+
+		int[] result = new int[intArr.size()];
+
+		for (int i = 0; i < intArr.size(); i++) {
+			result[i] = intArr.get(i);
+		}
+
+	}
+
+}
---
+++ PG_최대값최소값.java
@@ -0,0 +1,36 @@
+
+import java.util.Arrays;
+
+public class PG_최대값최소값 {
+   
+public String solution(String s) {
+        
+      String[] words1 = s.split(" ");
+       int[] words2 = new int[words1.length];
+   
+       for(int i = 0; i < words1.length; i++) {
+           words2[i] = Integer.parseInt(words1[i]);
+       }
+       Arrays.sort(words2);
+       String min = Integer.toString(words2[words2.length-1]);
+       String max = Integer.toString(words2[0]);
+   
+       return max+" "+min;
+    }
+   
+   public static void main(String[] args) {
+      String s = "-3 -2 -1 -4";
+      
+      String[] words1 = s.split(" ");
+       int[] words2 = new int[words1.length];
+   
+       for(int i = 0; i < words1.length; i++) {
+           words2[i] = Integer.parseInt(words1[i]);
+       }
+       Arrays.sort(words2);
+       String min = Integer.toString(words2[words2.length-1]);
+       String max = Integer.toString(words2[0]);
+        System.out.println(min+max);
+   }
+
+}
