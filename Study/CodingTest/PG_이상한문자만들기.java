9c879bf3fcc1b1f9442d4cee368b2a8557452c24
QLoooad <ckcc0813@gmail.com>
2023-02-08 15:20:06 +0900

    230208

--- Study/CodingTest/PG_이상한문자만들기.java
+++ Study/CodingTest/PG_이상한문자만들기.java
@@ -5,12 +5,19 @@ public class PG_이상한문자만들기 {
 	public static String solution(String s) {
 		
 		String changeS = s.toLowerCase();
+		
 		char[] charArr = changeS.toCharArray();
+		int count = 1;
 		
 		for(int i = 0; i < charArr.length; i++) {
-			if( i % 2 != 1 ) {
-				charArr[i] = (char)(charArr[i]-32);
+			if( count % 2 == 1 ) {
+				if(charArr[i] != ' '){//공백이 아니면 해당 번호 대문자로 변환 공백이면 count--로 하고 아무것도안함
+					charArr[i] = (char)(charArr[i]-32);
+				}else if(charArr[i] == ' '){
+					count--;
+				}
 			}
+			count++;
 		}
 		 String s2 = new String(charArr);
 		 
@@ -26,15 +33,17 @@ public class PG_이상한문자만들기 {
 		int count = 1;
 		
 		for(int i = 0; i < charArr.length; i++) {
-			if( i % 2 != 1 ) {
-				if(charArr[i] != ' '){//공백 확인
+			if( count % 2 == 1 ) {
+				if(charArr[i] != ' '){//공백 확인 공백이면 카운트 -1 후 카운트 숫자 기준으로 대문자 변환
 					charArr[i] = (char)(charArr[i]-32);
-					count++;
+				}else if(charArr[i] == ' '){
+					count--;
 				}
-				
 			}
+			count++;
 		}
 		 String s2 = new String(charArr);
+		 System.out.println(s2);
 		 System.out.println(solution(s));
 		 System.out.println("AbC AbCd AbCdE");
 	}
--- Study/CodingTest/PG_행렬덧셈.java
+++ Study/CodingTest/PG_행렬덧셈.java
@@ -3,13 +3,30 @@ package CodingTest;
 public class PG_행렬덧셈 {
 	
 	public int[][] solution(int[][] arr1, int[][] arr2) {
-        int[][] answer = {};
-        return answer;
+		
+		for(int i = 0; i < arr1.length; i++) {
+			for(int j = 0; j < arr1[0].length; j++) {
+				arr1[i][j] += arr2[i][j];
+			}
+		}
+        return arr1;
     }
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//int[][] arr = new int[rows][columns]; rows 가로 , columns 세로
+		
+		int[][] arr1 = { {0, 1, 2}, {3, 4, 5} }; // <<  arr1 [2][3]
+		int[][] arr2 = { {0, 1, 2}, {3, 4, 5} };
 
+		for(int i = 0; i < arr1.length; i++) {
+			for(int j = 0; j < arr1[0].length; j++) {
+				arr1[i][j] += arr2[i][j];
+			}
+		}
+		System.out.println(arr1.length);
+		System.out.println(arr1[0].length);
+		
+		
 	}
-
 }
