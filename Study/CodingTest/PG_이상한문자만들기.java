536fe2269dd39d14a28c49817578efa8183518c8
QLoooad <ckcc0813@gmail.com>
2023-02-08 13:31:13 +0900

    Merge remote-tracking branch 'origin/master'

--- Study/CodingTest/PG_이상한문자만들기.java
+++ Study/CodingTest/PG_이상한문자만들기.java
@@ -2,15 +2,41 @@ package CodingTest;
 
 public class PG_이상한문자만들기 {
 	
-	public String solution(String s) {
-        String answer = "";
-        return answer;
+	public static String solution(String s) {
+		
+		String changeS = s.toLowerCase();
+		char[] charArr = changeS.toCharArray();
+		
+		for(int i = 0; i < charArr.length; i++) {
+			if( i % 2 != 1 ) {
+				charArr[i] = (char)(charArr[i]-32);
+			}
+		}
+		 String s2 = new String(charArr);
+		 
+        return s2;
     }
 
 	public static void main(String[] args) {
 
+		String s = "abc abcd abcde";
+		String changeS = s.toLowerCase();
 		
-		//testss
+		char[] charArr = changeS.toCharArray();
+		int count = 1;
+		
+		for(int i = 0; i < charArr.length; i++) {
+			if( i % 2 != 1 ) {
+				if(charArr[i] != ' '){//공백 확인
+					charArr[i] = (char)(charArr[i]-32);
+					count++;
+				}
+				
+			}
+		}
+		 String s2 = new String(charArr);
+		 System.out.println(solution(s));
+		 System.out.println("AbC AbCd AbCdE");
 	}
 
 }
---
+++ Study/CodingTest/PG_행렬덧셈.java
@@ -0,0 +1,15 @@
+package CodingTest;
+
+public class PG_행렬덧셈 {
+	
+	public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = {};
+        return answer;
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
